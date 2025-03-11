ab146c4d4445ad030b068fe2320ff3709db59e0a
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-11 17:29:48 +0700

    write user controller show to show a user

--- rumbl/lib/rumbl_web/controllers/user_controller.ex
+++ rumbl/lib/rumbl_web/controllers/user_controller.ex
@@ -6,4 +6,9 @@ defmodule RumblWeb.UserController do
     users = Accounts.list_users()
     render(conn, :index, users: users)
   end
+
+  def show(conn, %{"id" => id}) do
+    user = Accounts.get_user(id)
+    render(conn, :show, user: user)
+  end
 end
