44362d302e283ff79ff6ac6a414ef907f19d7fb0
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-12 12:01:36 +0700

    update changeset rule when create/update user

--- rumbl/lib/rumbl/accounts/user.ex
+++ rumbl/lib/rumbl/accounts/user.ex
@@ -1,6 +1,8 @@
 defmodule Rumbl.Accounts.User do
   use Ecto.Schema
 
+  import Ecto.Changeset
+
   schema "users" do
     field(:name, :string)
     field(:username, :string)
@@ -8,5 +10,15 @@ defmodule Rumbl.Accounts.User do
     field(:password_hash, :string)
 
     timestamps()
+  end
+
+  @doc """
+  Function for change/update user
+  """
+  def changeset(user, params \\ %{}) do
+    user
+    |> cast(params, [:name, :username])
+    |> validate_required([:name, :username])
+    |> validate_length(:username, min: 1, max: 20)
   end
 end
