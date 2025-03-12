9a79009b9cd96019a1176ac6b33ef3a0d050d960
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-12 13:44:39 +0700

    define action create on user_controller

--- rumbl/lib/rumbl_web/controllers/user_controller.ex
+++ rumbl/lib/rumbl_web/controllers/user_controller.ex
@@ -17,4 +17,16 @@ defmodule RumblWeb.UserController do
     changeset = User.changeset(%User{})
     render(conn, :user_create, changeset: changeset)
   end
+
+  def create(conn, %{"user" => user_params}) do
+    case Accounts.create_user(user_params) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "User created successfully")
+        |> redirect(to: ~p"/users/#{user.id}")
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, :new, changeset: changeset)
+    end
+  end
 end
