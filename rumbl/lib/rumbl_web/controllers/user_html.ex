3e7b3f6bf0b8757126d4470e9b68466e5f90c00e
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-12 10:48:07 +0700

    define a component in heex template in user_html

--- rumbl/lib/rumbl_web/controllers/user_html.ex
+++ rumbl/lib/rumbl_web/controllers/user_html.ex
@@ -1,4 +1,15 @@
 defmodule RumblWeb.UserHTML do
   use RumblWeb, :html
   embed_templates("user_html/*")
+
+  @doc """
+  Render each user template
+  """
+  def user_show(assigns) do
+    ~H"""
+      <h1>User Profile</h1>
+      <p>Name: <strong><%= @user.name %></strong></p>
+      <p>Nickname: <%= @user.username %></p>
+    """
+  end
 end
