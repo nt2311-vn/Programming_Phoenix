94028f9809679019fb6206818b4d5920573765e9
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-12 13:56:27 +0700

    update user_controller actions

--- rumbl/lib/rumbl_web/controllers/user_controller.ex
+++ rumbl/lib/rumbl_web/controllers/user_controller.ex
@@ -3,21 +3,33 @@ defmodule RumblWeb.UserController do
   alias Rumbl.Accounts
   alias Rumbl.Accounts.User
 
+  @doc """
+  Index view list all user
+  """
   def index(conn, _params) do
     users = Accounts.list_users()
     render(conn, :index, users: users)
   end
 
+  @doc """
+  Show a specific user
+  """
   def show(conn, %{"id" => id}) do
     user = Accounts.get_user(id)
     render(conn, :show, user: user)
   end
 
+  @doc """
+  Template form on creating/changing user
+  """
   def new(conn, _params) do
     changeset = User.changeset(%User{})
     render(conn, :user_create, changeset: changeset)
   end
 
+  @doc """
+  Action on creating/changing user
+  """
   def create(conn, %{"user" => user_params}) do
     case Accounts.create_user(user_params) do
       {:ok, user} ->
