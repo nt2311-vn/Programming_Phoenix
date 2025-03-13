7ba4ec5ff91c670d0f679e0c3baf4090826e46d9
nt2311 <nguyentoan231196@gmail.com>
2025-03-13 21:37:33 +0700

    page live in live view render

--- pento/lib/pento_web/live/page_live.ex
+++ pento/lib/pento_web/live/page_live.ex
@@ -2,6 +2,6 @@ defmodule PentoWeb.PageLive do
   use PentoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :message, "Welcome to Liveview!")}
+    {:ok, assign(socket, query: "", result: %{})}
   end
 end
