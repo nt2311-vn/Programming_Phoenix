85a81c644625e5a42d67f964655fb0db93ef8e4d
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-14 11:16:35 +0700

    update liveview wrong_live module to track change diff in time

--- pento/lib/pento_web/live/wrong_live.ex
+++ pento/lib/pento_web/live/wrong_live.ex
@@ -2,13 +2,18 @@ defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Guest a number.")}
+    time = time()
+    {:ok, assign(socket, score: 0, message: "Guest a number.", time: time)}
   end
 
   def render(assigns) do
     ~H"""
     <h1>Your score: <%= @score %></h1>
-    <h2><%= @message %></h2>
+    <h2>
+      <%= @message %>
+      It's <%= @time %>
+    </h2>
+
     <h2>
       <%= for n <- 1..10 do %>
         <a href="#" phx-click="guess" phx-value-number={ n }><%= n %></a>
@@ -17,12 +22,17 @@ defmodule PentoWeb.WrongLive do
     """
   end
 
+  def time() do
+    DateTime.utc_now() |> to_string
+  end
+
   def handle_event("guess", %{"number" => guess} = data, socket) do
     IO.inspect(data)
 
     message = "Your guess #{guess}. Wrong guess again. "
     score = socket.assigns.score - 1
+    time = time()
 
-    {:noreply, assign(socket, message: message, score: score)}
+    {:noreply, assign(socket, message: message, score: score, time: time)}
   end
 end
