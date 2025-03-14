03e3f3d7284bfd43b89986bb96e0e62050e90c45
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-14 10:24:58 +0700

    write a the wrong live elixir for guess number game

---
+++ pento/lib/pento_web/live/wronge_live.ex
@@ -0,0 +1,19 @@
+defmodule PentoWeb.WrongeLive do
+  use PentoWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, score: 0, message: "Guest a number.")}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1>Your score { @score }</h1>
+    <h2>{ @message }</h2>
+    <h2>
+      <%= for n <- 1..10 do %>
+        <a href="#" phx-click="guess" phx-value-number={ n }>{ n }</a>
+      <% end %>
+    </h2>
+    """
+  end
+end
