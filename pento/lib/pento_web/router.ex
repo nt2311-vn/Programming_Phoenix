16fde0014654ce98e66e5348a798f0f7ea02e6f5
nt2311 <nguyentoan231196@gmail.com>
2025-03-15 21:16:24 +0700

    add products to router

--- pento/lib/pento_web/router.ex
+++ pento/lib/pento_web/router.ex
@@ -70,6 +70,12 @@ defmodule PentoWeb.Router do
   scope "/", PentoWeb do
     pipe_through([:browser, :require_authenticated_user])
 
+    live("/products", ProductLive.Index, :index)
+    live("/products/new", ProductLive.Index, :new)
+    live("/products/:id/edit", ProductLive.Index, :edit)
+    live("/products/:id", ProductLive.Show, :show)
+    live("/products/:id/show/edit", ProductLive.Show, :edit)
+
     live_session :require_authenticated_user,
       on_mount: [{PentoWeb.UserAuth, :ensure_authenticated}] do
       live("/users/settings", UserSettingsLive, :edit)
