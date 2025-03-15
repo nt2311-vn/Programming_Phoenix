e4e323e0abc84e665d42ed2e9a2ff52a63a2b8e1
nt2311 <nguyentoan231196@gmail.com>
2025-03-15 20:38:24 +0700

    redirect user if authenticated to /guess

--- pento/lib/pento_web/controllers/page_controller.ex
+++ pento/lib/pento_web/controllers/page_controller.ex
@@ -1,9 +1,19 @@
 defmodule PentoWeb.PageController do
   use PentoWeb, :controller
 
+  plug(:redirect_if_authenticated when action in [:home])
+
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
     render(conn, :home, layout: false)
+  end
+
+  defp redirect_if_authenticated(conn, _opts) do
+    if conn.assigns[:current_user] do
+      conn |> redirect(to: "/guess") |> halt()
+    else
+      conn
+    end
   end
 end
