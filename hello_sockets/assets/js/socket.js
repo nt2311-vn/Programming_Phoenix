8c12367a637196474f77dc86ce9e2c9c8f23deda
nt2311 <nguyentoan231196@gmail.com>
2025-03-16 22:02:39 +0700

    implement socket js client

--- hello_sockets/assets/js/socket.js
+++ hello_sockets/assets/js/socket.js
@@ -3,4 +3,14 @@ import { Socket } from "phoenix";
 let socket = new Socket("/socket", {});
 
 socket.connect();
+
 export default socket;
+
+const channel = socket.channel("ping")
+
+channel.join()
+  .receive("ok", resp => console.log("Joined ping", resp))
+  .receive("error", resp => console.log("Unable to join ping", resp))
+
+console.log("send ping")
+channel.push("ping").receive("ok", resp => console.log("receive", resp.ping))
