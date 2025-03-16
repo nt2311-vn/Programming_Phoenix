a858667e04acb9b1bc48cdefc3444633172054af
nt2311 <nguyentoan231196@gmail.com>
2025-03-16 15:57:52 +0700

    use wildcard to splat operator for all ping channel

--- hello_sockets/lib/hello_sockets_web/channels/user_socket.ex
+++ hello_sockets/lib/hello_sockets_web/channels/user_socket.ex
@@ -1,9 +1,11 @@
-use Phoenix.Socket
+defmodule HelloSocketsWeb.UserSocket do
+  use Phoenix.Socket
 
-channel("ping", HelloSocketsWeb.PingChanel)
+  channel("ping:*", HelloSocketsWeb.PingChanel)
 
-def connect(_params, socket, _connect_info) do
-  {:ok, socket}
+  def connect(_params, socket, _connect_info) do
+    {:ok, socket}
+  end
+
+  def id(_params), do: nil
 end
-
-def id, do: nil
