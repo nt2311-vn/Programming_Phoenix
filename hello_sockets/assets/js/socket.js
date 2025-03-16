cad318d3217d234c2e55db55908cc1b99fdeea08
nt2311 <nguyentoan231196@gmail.com>
2025-03-16 22:05:29 +0700

    test pong no reply and timeout messagde

--- hello_sockets/assets/js/socket.js
+++ hello_sockets/assets/js/socket.js
@@ -13,4 +13,11 @@ channel.join()
   .receive("error", resp => console.log("Unable to join ping", resp))
 
 console.log("send ping")
-channel.push("ping").receive("ok", resp => console.log("receive", resp.ping))
+channel.push("ping")
+  .receive("ok", resp => console.log("receive", resp.ping))
+
+console.log("send pong")
+channel.push("pong")
+  .receive("ok", resp => console.log("won't happen"))
+  .receive("error", resp => console.error("won't happen yet"))
+  .receive("timeout", resp => console.error("pong message timeout", resp))
