15d00df0876d97ea6cfdbc96782eaca2de6c2169
nt2311 <nguyentoan231196@gmail.com>
2025-03-16 21:37:48 +0700

    add handle income request with pong (noreply) and ding (shutdown server), add handle out request

--- hello_sockets/lib/hello_sockets_web/channels/ping_channel.ex
+++ hello_sockets/lib/hello_sockets_web/channels/ping_channel.ex
@@ -5,7 +5,26 @@ defmodule HelloSocketsWeb.PingChannel do
     {:ok, socket}
   end
 
-  def handle_in("ping", _payload, socket) do
-    {:reply, {:ok, %{ping: "pong"}}, socket}
+  def handle_in("ping", %{"ack_phrase" => ack_phrase}, socket) do
+    {:reply, {:ok, %{ping: ack_phrase}}, socket}
+  end
+
+  def handle_in("ping:" <> phrase, _payload, socket) do
+    {:reply, {:ok, %{ping: phrase}}, socket}
+  end
+
+  def handle_in("pong", _payload, socket) do
+    {:noreply, socket}
+  end
+
+  def handle_in("ding", _payload, socket) do
+    {:stop, :shutdown, {:ok, %{msg: "shutting down"}}, socket}
+  end
+
+  intercept("request_ping")
+
+  def handle_out("request_ping", payload, socket) do
+    push(socket, "send_ping", Map.put(payload, "from_node", Node.self()))
+    {:noreply, socket}
   end
 end
