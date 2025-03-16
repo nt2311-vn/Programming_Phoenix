8b09f82b3390e162289b6d1637f923672dd4c8ec
nt2311 <nguyentoan231196@gmail.com>
2025-03-16 22:02:34 +0700

    add an auto reply _payload pattern matching all income request

--- hello_sockets/lib/hello_sockets_web/channels/ping_channel.ex
+++ hello_sockets/lib/hello_sockets_web/channels/ping_channel.ex
@@ -5,6 +5,10 @@ defmodule HelloSocketsWeb.PingChannel do
     {:ok, socket}
   end
 
+  def handle_in("ping", _payload, socket) do
+    {:reply, {:ok, %{ping: "pong"}}, socket}
+  end
+
   def handle_in("ping", %{"ack_phrase" => ack_phrase}, socket) do
     {:reply, {:ok, %{ping: ack_phrase}}, socket}
   end
