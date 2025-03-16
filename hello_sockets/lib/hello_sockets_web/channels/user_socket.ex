a9fa4b5efc385a4356395c46a16a5f6a143bf114
nt2311 <nguyentoan231196@gmail.com>
2025-03-16 16:04:59 +0700

    define a communication of wildcard to wild channel in user socket

--- hello_sockets/lib/hello_sockets_web/channels/user_socket.ex
+++ hello_sockets/lib/hello_sockets_web/channels/user_socket.ex
@@ -2,6 +2,7 @@ defmodule HelloSocketsWeb.UserSocket do
   use Phoenix.Socket
 
   channel("ping:*", HelloSocketsWeb.PingChanel)
+  channel("wild:*", HelloSocketsWeb.WildCardChannel)
 
   def connect(_params, socket, _connect_info) do
     {:ok, socket}
