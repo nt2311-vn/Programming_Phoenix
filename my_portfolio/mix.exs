fe77dbe519a1a51d4705111e6852bf3e5e98ce97
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-27 17:18:53 +0700

    install dotenv

--- my_portfolio/mix.exs
+++ my_portfolio/mix.exs
@@ -58,7 +58,7 @@ defmodule MyPortfolio.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:dotenvy, "~> 0.8.0"}
+      {:dotenv, "~> 3.1.0", only: [:dev, :test]}
     ]
   end
 
