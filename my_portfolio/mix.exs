5d5048fe678b8c40423829519796be62dc3fbddc
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-27 16:34:45 +0700

    add .env to gitignore and install dotenvy package

--- my_portfolio/mix.exs
+++ my_portfolio/mix.exs
@@ -57,7 +57,8 @@ defmodule MyPortfolio.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:dotenvy, "~> 0.8.0"}
     ]
   end
 
