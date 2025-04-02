ca66f8da63a6ef41452ba781f33de60edded0470
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-02 22:30:22 +0700

    create a dir for embed tempalates heex and using embed_templates function to map the controller view to embed template files

--- shop/lib/shop_web/controllers/product_html.ex
+++ shop/lib/shop_web/controllers/product_html.ex
@@ -1,9 +1,5 @@
 defmodule ShopWeb.ProductHTML do
   use ShopWeb, :html
 
-  def index(assigns) do
-    ~H"""
-    <h1>This is the product listing page!</h1>
-    """
-  end
+  embed_templates("product_html/*")
 end
