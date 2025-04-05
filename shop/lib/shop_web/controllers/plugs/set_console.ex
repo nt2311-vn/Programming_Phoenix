aa78aee9c7fa99c9a9d865d556e964e7dc2c32fa
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-05 19:53:58 +0700

    guard the input value to ensure the input is predicted one

--- shop/lib/shop_web/controllers/plugs/set_console.ex
+++ shop/lib/shop_web/controllers/plugs/set_console.ex
@@ -1,6 +1,7 @@
 defmodule ShopWeb.Plugs.SetConsole do
   import Plug.Conn
 
+  @valid_consoles ["nitendo", "playstation", "pc", "xbox"]
   def init(default_console), do: default_console
 
   def call(
@@ -9,14 +10,22 @@ defmodule ShopWeb.Plugs.SetConsole do
             "console" => console
           }
         } = conn,
-        _opts
-      ) do
+        _default_console
+      )
+      when console in @valid_consoles do
     conn
     |> assign(:console, console)
     |> put_resp_cookie("console", console, max_age: :timer.hours(24) * 30)
   end
 
+  def call(%Plug.Conn{:cookies => %{"console" => console}} = conn, _default_console)
+      when console in @valid_consoles do
+    conn |> assign(:console, console)
+  end
+
   def call(%Plug.Conn{} = conn, default_console) do
-    conn |> assign(:console, default_console)
+    conn
+    |> assign(:console, default_console)
+    |> put_resp_cookie("console", default_console, max_age: :timer.hours(24) * 3)
   end
 end
