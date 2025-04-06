79a3a20624318d5c2acb8dc1c4b6fcaba875dd5f
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-06 09:11:07 +0700

    product html write production function component

--- shop/lib/shop_web/controllers/product_html.ex
+++ shop/lib/shop_web/controllers/product_html.ex
@@ -2,4 +2,11 @@ defmodule ShopWeb.ProductHTML do
   use ShopWeb, :html
 
   embed_templates("product_html/*")
+
+  def product(assigns) do
+    ~H"""
+    <h1>Game <%= assigns.name %></h1>
+    <p>Product id <%= assigns.id %></p>
+    """
+  end
 end
