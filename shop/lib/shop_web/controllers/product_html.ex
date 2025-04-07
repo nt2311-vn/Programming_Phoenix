1fa725ff11626d5e01c2a4bebc2df024f13249cc
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-07 10:00:33 +0700

    change the pass param into product

--- shop/lib/shop_web/controllers/product_html.ex
+++ shop/lib/shop_web/controllers/product_html.ex
@@ -1,15 +1,17 @@
 defmodule ShopWeb.ProductHTML do
   use ShopWeb, :html
 
+  alias Shop.Product
+
   embed_templates("product_html/*")
 
-  attr(:name, :string, required: true)
-  attr(:id, :string, required: true)
+  attr(:product, Product, required: true)
 
   def product(assigns) do
     ~H"""
-    <h1>Game {@name}</h1>
-    <p>Product id {@id}</p>
+    <h1>Game {@product.name}</h1>
+    <p>Product id {@product.id}</p>
+    <p>Game slug {@product.slug}</p>
     """
   end
 end
